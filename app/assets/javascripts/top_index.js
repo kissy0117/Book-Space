8f5bab34a2772b94b2b6ecbb6d41b3d75887b416
kissy0117 <yk.supercell@gmail.com>
2020-04-06 14:07:23 +0900

    posts.js edit

--- app/assets/javascripts/top_index.js
+++ app/assets/javascripts/top_index.js
@@ -2,32 +2,63 @@ $(function(){
   
   $('#searchBtn').on("click", function(){
     let title = $("#title").val();
-    
-    const url = "https://www.googleapis.com/books/v1/volumes?q=intitle:" + title ;
+    let author = $("#author").val();
 
+    if(title == "" && author == ""){
+      alert("検索条件を入力してください");
+    }else{
 
-    $.getJSON(url, function(){
-      console.log("実行");
-      console.log(url);
-    })
+      if(title == ""){
+        const url = `https://www.googleapis.com/books/v1/volumes?q=inauthor:${author}`;
+        $.getJSON(url, function(){
+          console.log("実行");
+          console.log(url);
+        })
+        .done(function(json) {
+          $.each(json.items, function(index, item){
+          console.log(item);
+          console.log("タイトル：" + item.volumeInfo.title + "/発売日：" + item.volumeInfo.publishedDate + "/画像リンク" + item.volumeInfo.imageLinks.smallThumbnail);
+          $("#sampleID").html('<img src="' + item.volumeInfo.imageLinks.smallThumbnail + '" />')
+          })
+        })
+        .fail(function(){
+          alert('error');
+        });
 
-    .done(function(json) {
-      $.each(json.items, function(index, item){
-
-      console.log(item);
-      console.log("タイトル：" + item.volumeInfo.title + "/発売日：" + item.volumeInfo.publishedDate + "/画像リンク" + item.volumeInfo.imageLinks.smallThumbnail);
-      
-      
-      $("#sampleID").html('<img src="' + item.volumeInfo.imageLinks.smallThumbnail + '" />')
-      
-      
-
-
-    })
-  })
-
-    .fail(function(){
-      alert('error');
-    });
+      }else if(author == ""){
+        const url = `https://www.googleapis.com/books/v1/volumes?q=intitle:${title}`;
+        $.getJSON(url, function(){
+          console.log("実行");
+          console.log(url);
+        })
+        .done(function(json) {
+          $.each(json.items, function(index, item){
+          console.log(item);
+          console.log("タイトル：" + item.volumeInfo.title + "/発売日：" + item.volumeInfo.publishedDate + "/画像リンク" + item.volumeInfo.imageLinks.smallThumbnail);
+          
+          $("#sampleID").html('<img src="' + item.volumeInfo.imageLinks.smallThumbnail + '" />')
+          })
+        })
+        .fail(function(){
+          alert('error');
+        });
+      }else{
+        const url = `https://www.googleapis.com/books/v1/volumes?q=intitle:${title}+inauthor:${author}`;
+        $.getJSON(url, function(){
+          console.log("実行");
+          console.log(url);
+        })
+        .done(function(json) {
+          $.each(json.items, function(index, item){
+          console.log(item);
+          console.log("タイトル：" + item.volumeInfo.title + "/発売日：" + item.volumeInfo.publishedDate + "/画像リンク" + item.volumeInfo.imageLinks.smallThumbnail);
+          $("#sampleID").html('<img src="' + item.volumeInfo.imageLinks.smallThumbnail + '" />')
+          })
+        })
+        .fail(function(){
+          alert('error');
+        });
+      }
+    }
   })
 });
