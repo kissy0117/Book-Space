60eb9452af869380347c83a0496dc61f709ba2cd
kissy0117 <yk.supercell@gmail.com>
2020-04-10 15:04:40 +0900

    View create users show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -41,7 +41,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:image, :title, :author, :infomation, :content).merge(user_id: current_user.id)
+    params.require(:post).permit(:image, :title, :author, :infomation, :content, :remove_image).merge(user_id: current_user.id)
   end
 
 end
