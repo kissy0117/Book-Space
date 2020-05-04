166d1045e9def60e3a4f13cb443c39b00fd9369d
kissy0117 <yk.supercell@gmail.com>
2020-05-04 09:01:38 +0900

    gem install

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 describe User do
   describe '#create' do
     it "nameがない場合は登録できないこと" do
-      user = build( email: "test@lookout", name: "", image: "qqq")
+      user = User.new( email: "test@lookout", name: "", image: "qqq")
       user.valid?
       expect(user.errors[:name]).to include("can't be blank")
     end
